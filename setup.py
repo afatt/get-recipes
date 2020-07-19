49744e52408b3940195f5afad852cfbee84ec5de
unknown <afatt90@gmail.com>
2020-07-18 23:05:15 -0400

    Adds setup.py to install dependencies

---
+++ setup.py
@@ -0,0 +1,28 @@
+import sys
+from setuptools import setup, find_packages
+
+__version__ = '1.0.0'
+
+REQUIRES = [
+    'Pillow==7.2.0',
+    'pypng==0.0.20',
+    'pyzbar==0.1.8',
+    'selenium==3.141.0',
+    'urllib3==1.25.9'
+]
+
+setup(
+    name='Get Recipes',
+    author='Austin Fatt',
+    author_email='afatt90@gmail.com',
+    url='https://github.com/afatt/get-recipes',
+    version=__version__,
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='Trader',
+    packages=find_packages(),
+    install_requires=REQUIRES
+)
